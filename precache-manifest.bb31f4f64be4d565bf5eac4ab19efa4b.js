6ab73c5d50646c3cf7aa187dc59bb8a157e4da7f
Jimmy Wen <JimmyWen@Jimmys-MacBook.local>
2019-10-23 00:25:34 +0800

    Updates

---
+++ precache-manifest.bb31f4f64be4d565bf5eac4ab19efa4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb2e19d3582ec7cc97b655264ac303c",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "969bd3574112993ddf8a",
+    "url": "/react-todo/static/css/main.fcf9b4fa.chunk.css"
+  },
+  {
+    "revision": "7aff60e2d9f1b2089ebe",
+    "url": "/react-todo/static/js/2.3921579e.chunk.js"
+  },
+  {
+    "revision": "969bd3574112993ddf8a",
+    "url": "/react-todo/static/js/main.2a2238f7.chunk.js"
+  },
+  {
+    "revision": "db31805f620bdbd554e9",
+    "url": "/react-todo/static/js/runtime-main.cc8f0ef0.js"
+  }
+]);
